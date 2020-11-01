a23270ea9ff17cb8233b8646b61c82ced2bab95e
TengfengHuang <760201280@qq.com>
2020-11-01 14:16:32 +0800

    initialize

--- app/Views/oneplace.php
+++ app/Views/oneplace.php
@@ -1,15 +1,20 @@
 <!doctype html>
 <html>
 	<head>
-		<title>DGPT4711 Lab 04</title>
+		<title>comp4711_Assignment1</title>
 	</head>
 	<body>
-		<h1>One Travel Destinations</h1>
+		<h1>This is a {Country_from} car</h1>
 		<p><strong>ID:</strong> {id}</p>
 		<p><strong>Name:</strong> {name}</p>
-		<p><strong>Description:</strong> {description}</p>
+		<p><strong>Country place:</strong> {Country_place}</p>
+                <p><strong>description:</strong> {description}</p>
 		<p><strong>Tourism site:</strong> {link}</p>
-		<p><img src="/image/{image}"/></p>
+                <p><strong>hoursepower:</strong> {hoursepower}</p>
+                <p><strong>engine type:</strong> {engine_type}</p>
+                <p><strong>gearbox:</strong> {gearbox}</p>
+                <p><strong>acceleration time:</strong> {acceleration_time} (0-100km/h) </p>
+                <p><img src="/image/{image}" width="360" height="240"/></p>
 		<p><a href="/home">Home</a></p>
 	</body>
 </html>
